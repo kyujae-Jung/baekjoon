ea67108a14f4ca9c5437e3fcdc2e2c5803e332d9
kyujae-Jung <113874252+kyujae-Jung@users.noreply.github.com>
2023-02-12 10:47:03 +0900

    [Bronze IV] Title: !밀비 급일, Time: 124 ms, Memory: 14056 KB -BaekjoonHub

---
+++ 백준/Bronze/11365. ！밀비 급일/！밀비 급일.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringBuilder sb = new StringBuilder();
+
+        while (true) {
+            String s = br.readLine();
+
+            if (s.equals("END")) break;
+
+            StringBuilder reverse = new StringBuilder(s);
+
+            reverse = reverse.reverse();
+
+            sb.append(reverse + "\n");
+        }
+        System.out.print(sb);
+    }
+}
