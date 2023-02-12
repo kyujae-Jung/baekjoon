04a53ca3cf3fd976fab358ebba74df3e5bc49014
kyujae-Jung <113874252+kyujae-Jung@users.noreply.github.com>
2023-02-12 10:48:59 +0900

    [Bronze II] Title: 음계, Time: 120 ms, Memory: 14224 KB -BaekjoonHub

---
+++ 백준/Bronze/2920. 음계/음계.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int[] arr = new int[8];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < 8; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        boolean asc = true;
+
+        boolean dsc = true;
+
+        for (int i = 0; i < 7; i++) {
+            //다음 숫자가 더 크면 descending은 안 된다
+            if (arr[i+1] > arr[i]) dsc = false;
+            //다음 숫자가 더 작으면 ascending은 안 된다
+            if (arr[i+1] < arr[i]) asc = false;
+        }
+        if (asc) {
+            System.out.print("ascending");
+        } else if (dsc) {
+            System.out.print("descending");
+        } else {
+            //asc, dsc 둘 다 아니면 mixed 출력
+            System.out.print("mixed");
+        }
+    }
+}
