bf08189db3b616c5e17c4bf1e6dd9122ab48a5ea
kyujae-Jung <113874252+kyujae-Jung@users.noreply.github.com>
2023-04-06 23:32:34 +0900

    [Silver II] Title: 유기농 배추, Time: 164 ms, Memory: 16124 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.java
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.java
@@ -1,69 +1,59 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.StringTokenizer;
 
 public class Main {
-    private static int M;
+    static int M,N;
+    static int[][] arr;
+    static boolean[][] check;
 
-    private static int N;
-
-    private static int[][] arr;
-
-    private static boolean[][] check;
-
-
-    public static void main(String[] args) throws IOException
-    {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
         StringTokenizer st;
 
         int T = Integer.parseInt(br.readLine());
 
-        for (int i = 0; i < T; i++)
-        {
+        for (int i=0; i<T; i++) {
             st = new StringTokenizer(br.readLine());
-
             M = Integer.parseInt(st.nextToken());
             N = Integer.parseInt(st.nextToken());
             int K = Integer.parseInt(st.nextToken());
 
             arr = new int[M][N];
             check = new boolean[M][N];
-            int bugs = 0;
+            int count = 0;
 
-            for (int j = 0; j < K; j++)
-            {
+            for (int j=0; j<K; j++) {
                 st = new StringTokenizer(br.readLine());
-                int x = Integer.parseInt(st.nextToken());
-                int y = Integer.parseInt(st.nextToken());
-                arr[x][y] = 1;
+                int a = Integer.parseInt(st.nextToken());
+                int b = Integer.parseInt(st.nextToken());
+
+                arr[a][b] = 1;
             }
 
-            for (int y = 0; y < N; y++) {
-                for (int x = 0; x < M; x++) {
+            for (int y=0; y<N; y++) {
+                for (int x=0; x<M; x++) {
                     if (arr[x][y] == 1 && !check[x][y]){
-                        bugs++;
-                        dfs(x, y);
+                        count++;
+                        DFS(x, y);
                     }
                 }
             }
-            System.out.println(bugs);
+            sb.append(count).append("\n");
         }
-        br.close();
+        System.out.println(sb);
     }
 
-    private static void dfs(int row, int col){
-    int[] dx = { 0, 0, -1, 1 };
-    int[] dy = { -1, 1, 0, 0 };
-    check[row][col] = true;
-    for (int i = 0; i < 4; i++){
-        int xn = row + dx[i];
-        int yn = col + dy[i];
+    public static void DFS(int x, int y) {
+        int[] dx = {0,0,-1,1};
+        int[] dy = {-1,1,0,0};
+        check[x][y] = true;
+        for (int i=0; i<4; i++) {
+            int xn = x + dx[i];
+            int yn = y + dy[i];
 
-        if ((xn > -1 && xn < M) && (yn > -1 && yn < N) && arr[xn][yn] == 1 && !check[xn][yn]) {
-            dfs(xn, yn);
+            if (xn < M && xn > -1 && yn < N && yn > -1 && arr[xn][yn] == 1 && !check[xn][yn])
+                DFS(xn, yn);
         }
-    }
     }
 }
