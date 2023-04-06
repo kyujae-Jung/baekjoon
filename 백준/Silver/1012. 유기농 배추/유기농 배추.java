324a3774c9ed90eb858746744917e2909d0d0ac9
kyujae-Jung <113874252+kyujae-Jung@users.noreply.github.com>
2023-04-06 22:49:28 +0900

    [Silver II] Title: 유기농 배추, Time: 164 ms, Memory: 16228 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.java
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.java
@@ -1,85 +1,69 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-public class Main
-{
+public class Main {
     private static int M;
-    
+
     private static int N;
-    
-    private static int[][] area;
-    
-    private static boolean[][] isVisit;
-    
+
+    private static int[][] arr;
+
+    private static boolean[][] check;
+
+
     public static void main(String[] args) throws IOException
     {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        
-        int T = Integer.parseInt(reader.readLine());
-        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int T = Integer.parseInt(br.readLine());
+
         for (int i = 0; i < T; i++)
         {
-            String[] temp = reader.readLine().split(" ");
-            
-            M = Integer.parseInt(temp[0]);
-            
-            N = Integer.parseInt(temp[1]);
-            
-            int K = Integer.parseInt(temp[2]);
-            
-            area = new int[M][N];
-            
-            isVisit = new boolean[M][N];
-            
+            st = new StringTokenizer(br.readLine());
+
+            M = Integer.parseInt(st.nextToken());
+            N = Integer.parseInt(st.nextToken());
+            int K = Integer.parseInt(st.nextToken());
+
+            arr = new int[M][N];
+            check = new boolean[M][N];
             int bugs = 0;
-            
+
             for (int j = 0; j < K; j++)
             {
-                temp = reader.readLine().split(" ");
-                
-                int x = Integer.parseInt(temp[0]);
-                int y = Integer.parseInt(temp[1]);
-                
-                area[x][y] = 1;
+                st = new StringTokenizer(br.readLine());
+                int x = Integer.parseInt(st.nextToken());
+                int y = Integer.parseInt(st.nextToken());
+                arr[x][y] = 1;
             }
-            
-            for (int y = 0; y < N; y++)
-            {
-                for (int x = 0; x < M; x++)
-                {
-                    if (area[x][y] == 1 && !isVisit[x][y])
-                    {
+
+            for (int y = 0; y < N; y++) {
+                for (int x = 0; x < M; x++) {
+                    if (arr[x][y] == 1 && !check[x][y]){
                         bugs++;
-                        
                         dfs(x, y);
                     }
                 }
             }
-            
             System.out.println(bugs);
         }
-        
-        reader.close();
+        br.close();
     }
-    
-    private static void dfs(int x, int y)
-    {
-        int[] dx = { 0, 0, -1, 1 };
-        
-        int[] dy = { -1, 1, 0, 0 };
-        
-        isVisit[x][y] = true;
-        
-        for (int i = 0; i < 4; i++)
-        {
-            int xn = x + dx[i];
-            int yn = y + dy[i];
-            
-            if ((xn > -1 && xn < M) && (yn > -1 && yn < N) && area[xn][yn] == 1 && !isVisit[xn][yn])
-            {
-                dfs(xn, yn);
-            }
+
+    private static void dfs(int row, int col){
+    int[] dx = { 0, 0, -1, 1 };
+    int[] dy = { -1, 1, 0, 0 };
+    check[row][col] = true;
+    for (int i = 0; i < 4; i++){
+        int xn = row + dx[i];
+        int yn = col + dy[i];
+
+        if ((xn > -1 && xn < M) && (yn > -1 && yn < N) && arr[xn][yn] == 1 && !check[xn][yn]) {
+            dfs(xn, yn);
         }
+    }
     }
 }
